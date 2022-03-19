d302cded12bd571d09cbf8a324347d9994b49154
andrey7373 <100881494+andrey7373@users.noreply.github.com>
2022-03-20 03:41:08 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,48 +1,24 @@
 #!/bin/bash
 
-if [[ -z "${PASSWORD}" ]]; then
-  export PASSWORD="5c301bb8-6c77-41a0-a606-4ba11bbab084"
-fi
-echo ${PASSWORD}
+#Шаг 2 — Создание центра сертификации
+	mkdir -p ~/pki/{cacerts,certs,private}
+	chmod 700 ~/pki
 
-export PASSWORD_JSON="$(echo -n "$PASSWORD" | jq -Rc)"
+#Сгенерировать ключ root. Это будет 4096-битный ключ RSA, который будет использоваться для подписи корневого центра сертификации.
+	ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/ca-key.pem
+#созданию корневого центра сертификации, используя ключ для подписания сертификата root:	
+	ipsec pki --self --flag serverAuth --in ~/pki/private/ca-key.pem --type rsa --digest sha256 --dn "C=US, O=Example Company, CN=alpatski.asuscomm.com" --outform pem  --ca --lifetime 3653 > ~/pki/cacerts/ca-cert.pem
 
-if [[ -z "${ENCRYPT}" ]]; then
-  export ENCRYPT="chacha20-ietf-poly1305"
-fi
+#Шаг 3 — Генерирование сертификата для сервера VPN
+#создадим сертификат и ключ для сервера VPN. Этот сертификат позволит клиентам проверять подлинность сервера, используя только что сгенерированный нами сертификат CA.
+	
+#Вначале создайте закрытый ключ сервера VPN с помощью следующей команды:
+	ipsec pki --gen --type rsa --size 4096 --outform pem > ~/pki/private/server-key.pem
+	ipsec pki --pub --in ~/pki/private/server-key.pem --type rsa > ~/pki/private/server-key.csr
+	
+#Затем создайте и подпишите сертификат сервера VPN, используя ключ центра сертификации, созданный на предыдущем шаге:
+	ipsec pki --issue --cacert ~/pki/cacerts/ca-cert.pem --cakey ~/pki/private/ca-key.pem --digest sha256 --dn "C=US, O=Example Company, CN=alpatski.asuscomm.com" --san "alpatski.asuscomm.com" --flag serverAuth --outform pem < ~/pki/private/server-key.csr > ~/pki/certs/server-cert.pem
+	openssl rsa -in ~/pki/private/server-key.pem -out ~/pki/private/server-key.der -outform DER
 
-if [[ -z "${QR_Path}" ]]; then
-  export QR_Path="/qr_img"
-fi
-echo ${QR_Path}
-
-case "$AppName" in
-	*.*)
-		export DOMAIN="$AppName"
-		;;
-	*)
-		export DOMAIN="$AppName.herokuapp.com"
-		;;
-esac
-
-bash /conf/shadowsocks-libev_config.json >  /etc/shadowsocks-libev/config.json
-echo /etc/shadowsocks-libev/config.json
-cat /etc/shadowsocks-libev/config.json
-
-bash /conf/nginx_ss.conf > /etc/nginx/conf.d/ss.conf
-echo /etc/nginx/conf.d/ss.conf
-cat /etc/nginx/conf.d/ss.conf
-
-
-if [ "$AppName" = "no" ]; then
-  echo "Do not generate QR-code"
-else
-  [ ! -d /wwwroot/${QR_Path} ] && mkdir /wwwroot/${QR_Path}
-  ss="ss://$(echo -n ${ENCRYPT}:${PASSWORD} | base64 -w 0)@${DOMAIN}:443" 
-  echo "${ss}" | tr -d '\n' > /wwwroot/${QR_Path}/index.html
-  echo -n "${ss}" | qrencode -s 6 -o /wwwroot/${QR_Path}/vpn.png
-fi
-
-ss-server -c /etc/shadowsocks-libev/config.json &
-rm -rf /etc/nginx/sites-enabled/default
-nginx -g 'daemon off;'
+#Теперь мы сгенерировали все файлы TLS/SSL, необходимые StrongSwan, и можем переместить их в каталог /etc/ipsec.d следующим образом:	
+	cp -r ~/pki/* /etc/ipsec.d/
